7159b4334352bcf26da5cacf5f1594047f09fac2
MihaiCovasan <92455723+MihaiCovasan@users.noreply.github.com>
2023-01-06 10:31:31 +0200

    Add project files.

---
+++ API_Aplication/Controllers/OldRecipeController.cs
@@ -0,0 +1,85 @@
+﻿using API_Aplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Aplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OldRecipeController : Controller
+    {
+        [HttpPost]
+        public ActionResult CreateRecipes([FromBody] Recipe newRecipe)
+        {
+            // Create logic => return true / ok
+            var successfullyCreated = true;
+
+            if (!successfullyCreated)
+            {
+                return BadRequest();
+            }
+
+            return Created("[controller]", newRecipe);
+        }
+
+        [HttpGet]
+        public ActionResult GetRecipes([FromQuery] int count)
+        {
+            Recipe[] recipes =
+            {
+                new() {Title = "Recipe1", Description = "Recipe1 description"},
+                new() {Title = "Recipe2", Description = "Recipe1 description"},
+                new() {Title = "Recipe3", Description = "Recipe1 description"},
+            };
+
+            // Get recipes
+            if (recipes == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(recipes.Take(count));
+        }
+
+        [HttpDelete("{id}")] // api/Recipe/50
+        public ActionResult DeleteRecipes(int id)
+        {
+            // Delete logic => return true / ok
+            var successfullyDeleted = true;
+
+            if (!successfullyDeleted)
+            {
+                return BadRequest();
+            }
+
+            return NoContent();
+        }
+
+        [HttpPut("entire/{id}")]
+        public ActionResult UpdateEntireRecipes(int id)
+        {
+            // Update logic - entire entity / object / model
+            var successfullyUpdated = true;
+
+            if (!successfullyUpdated)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        [HttpPatch("partial/{id}")]
+        public ActionResult UpdatePartialRecipes(int id)
+        {
+            // Update logic - needed field / properites for the entity / object / model
+            var successfullyUpdated = true;
+
+            if (!successfullyUpdated)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}
---
+++ API_Aplication/Data/API_AplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using API_Aplication.Models;
+
+namespace API_Aplication.Data
+{
+    public class API_AplicationContext : DbContext
+    {
+        public API_AplicationContext (DbContextOptions<API_AplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<API_Aplication.Models.Recipe> Recipe { get; set; } = default!;
+    }
+}
